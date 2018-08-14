8a249c89afb1225d131d68c4beae6abc5b3b41e7
limaleandro1999 <limaleandro1999@gmail.com>
2018-08-14 17:53:33 -0300

    adicionando comentários

--- queries.js
+++ queries.js
@@ -6,7 +6,7 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var db = pgp(process.env.DATABASE_URL || 'postgres://postgres:root@localhost:5432/loja');
+var db = pgp(process.env.DATABASE_URL || 'postgres://postgres:root@localhost:5432/loja'); //realiza a conexao com o banco de dados usando a variavel de ambiente DATABASE_URL, ou caso nao tenha sido configurada utiliza a string de conexao
 
 module.exports = {
     getVendasPorVendedor: getVendasPorVendedor,
@@ -14,7 +14,7 @@ module.exports = {
     getProdutosMaisVendidos: getProdutosMaisVendidos
 };
 
-function getVendasPorVendedor(req, res, next){
+function getVendasPorVendedor(req, res, next){//faz a consulta no banco e adiciona a um json as informacoes do numero de vendas agrupadas por vendedores 
     var now = new Date();
 	var mesEAno = now.getFullYear() +'-'+ (now.getMonth()+1); //mês e ano atual
 	const sql = "select vendedor, count(vendas.id) as total_de_vendas from vendas where data_venda between '"+ mesEAno +"-01' and '"+ mesEAno +"-30' group by vendedor order by total_de_vendas desc;";
@@ -32,7 +32,7 @@ function getVendasPorVendedor(req, res, next){
         });
 }
 
-function getClientesPorProduto(req, res, next){
+function getClientesPorProduto(req, res, next){ //faz a consulta no banco e adiciona a um json as informacoes dos clientes que mais compraram um produto e o numero de compras 
     var produtoId = parseInt(req.params.id); // transforma em int, para evitar sql injection
     const sql = 'select nome, count(venda_id) as total_de_produtos from vendas_produtos inner join vendas on vendas_produtos.venda_id = vendas.id inner join clientes on vendas.cliente_id = clientes.id where vendas_produtos.produto_id = '+ produtoId +' group by clientes.nome order by count(venda_id) desc;';
     db.any(sql)
@@ -49,7 +49,7 @@ function getClientesPorProduto(req, res, next){
         });
 }
 
-function getProdutosMaisVendidos(req, res, next){
+function getProdutosMaisVendidos(req, res, next){ //faz a consulta no banco e adiciona a um json as informacoes dos produtos mais vendidos no mês e o total de vendas
     var now = new Date();
 	var mesEAno = now.getFullYear() +'-'+ (now.getMonth()+1); //mês e ano atual
     const sql = "select descricao, count(produto_id) as total_de_vendas from vendas_produtos inner join produtos on vendas_produtos.produto_id = produtos.id inner join vendas on vendas_produtos.venda_id = vendas.id where data_venda between '"+ mesEAno +"-01' and '"+ mesEAno +"-30' group by descricao order by total_de_vendas desc;";
--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ router.get('/', function(req, res, next) {
 
 var db = require('../queries');
 
-router.get('/api/vendas-por-vendedor', db.getVendasPorVendedor);
-router.get('/api/clientes-por-produto/:id', db.getClientesPorProduto);
-router.get('/api/produtos-mais-vendidos', db.getProdutosMaisVendidos);
+router.get('/api/vendas-por-vendedor', db.getVendasPorVendedor); //retorna no response um json com as informacoes do numero de vendas agrupadas por vendedores 
+router.get('/api/clientes-por-produto/:id', db.getClientesPorProduto); //retorna no response um json com as informacoes dos clientes que mais compraram um produto e o numero de compras 
+router.get('/api/produtos-mais-vendidos', db.getProdutosMaisVendidos); //retorna no response um json com as informacoes dos produtos mais vendidos no mês e o total de vendas
 
 module.exports = router;
